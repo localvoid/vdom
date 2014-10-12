7318df6fca380b092f366f2ab0424d16d0dc2ea6
Boris Kaul <localvoid@gmail.com>
2014-10-12 17:00:47 +0700

    Add high-level API for DOM creation

---
+++ lib/src/api.dart
@@ -0,0 +1,335 @@
+// Copyright (c) 2014, the VDom project authors. Please see the AUTHORS file for
+// details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+part of vdom;
+
+Element element(Object key, String tag, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, tag, children, attributes, classes);
+}
+
+Element a(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'a', children, attributes, classes);
+}
+
+Element area(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'area', children, attributes, classes);
+}
+
+Element br(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'br', children, attributes, classes);
+}
+
+Element base(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'base', children, attributes, classes);
+}
+
+Element body(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'body', children, attributes, classes);
+}
+
+Element button(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'button', children, attributes, classes);
+}
+
+Element canvas(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'canvas', children, attributes, classes);
+}
+
+Element content(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'content', children, attributes, classes);
+}
+
+Element dl(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'dl', children, attributes, classes);
+}
+
+Element dt(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'dt', children, attributes, classes);
+}
+
+Element dd(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'dd', children, attributes, classes);
+}
+
+Element details(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'details', children, attributes, classes);
+}
+
+Element dialog(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'dialog', children, attributes, classes);
+}
+
+Element div(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'div', children, attributes, classes);
+}
+
+Element embed(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'embed', children, attributes, classes);
+}
+
+Element fieldset(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'fieldset', children, attributes, classes);
+}
+
+Element form(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'form', children, attributes, classes);
+}
+
+Element hr(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'hr', children, attributes, classes);
+}
+
+Element head(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'head', children, attributes, classes);
+}
+
+Element h1(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'h1', children, attributes, classes);
+}
+
+Element h2(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'h2', children, attributes, classes);
+}
+
+Element h3(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'h3', children, attributes, classes);
+}
+
+Element h4(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'h4', children, attributes, classes);
+}
+
+Element h5(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'h5', children, attributes, classes);
+}
+
+Element h6(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'h6', children, attributes, classes);
+}
+
+Element iframe(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'iframe', children, attributes, classes);
+}
+
+Element img(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'img', children, attributes, classes);
+}
+
+Element input(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'input', children, attributes, classes);
+}
+
+Element keygen(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'keygen', children, attributes, classes);
+}
+
+Element li(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'li', children, attributes, classes);
+}
+
+Element label(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'label', children, attributes, classes);
+}
+
+Element legend(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'legend', children, attributes, classes);
+}
+
+Element link(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'link', children, attributes, classes);
+}
+
+Element map(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'map', children, attributes, classes);
+}
+
+Element media(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'media', children, attributes, classes);
+}
+
+Element menu(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'menu', children, attributes, classes);
+}
+
+Element meta(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'meta', children, attributes, classes);
+}
+
+Element meter(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'meter', children, attributes, classes);
+}
+
+Element mod(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'mod', children, attributes, classes);
+}
+
+Element ol(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'ol', children, attributes, classes);
+}
+
+Element object(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'object', children, attributes, classes);
+}
+
+Element optgroup(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'optgroup', children, attributes, classes);
+}
+
+Element option(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'option', children, attributes, classes);
+}
+
+Element output(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'output', children, attributes, classes);
+}
+
+Element p(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'p', children, attributes, classes);
+}
+
+Element param(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'param', children, attributes, classes);
+}
+
+Element picture(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'picture', children, attributes, classes);
+}
+
+Element pre(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'pre', children, attributes, classes);
+}
+
+Element progress(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'progress', children, attributes, classes);
+}
+
+Element quote(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'quote', children, attributes, classes);
+}
+
+Element select(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'select', children, attributes, classes);
+}
+
+Element source(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'source', children, attributes, classes);
+}
+
+Element span(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'span', children, attributes, classes);
+}
+
+Element caption(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'caption', children, attributes, classes);
+}
+
+Element td(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'td', children, attributes, classes);
+}
+
+Element tr(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'tr', children, attributes, classes);
+}
+
+Element table(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'table', children, attributes, classes);
+}
+
+Element template(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'template', children, attributes, classes);
+}
+
+Element textarea(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'textarea', children, attributes, classes);
+}
+
+Element ul(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'ul', children, attributes, classes);
+}
+
+Element article(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'article', children, attributes, classes);
+}
+
+Element section(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'section', children, attributes, classes);
+}
+
+Element nav(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'nav', children, attributes, classes);
+}
+
+Element header(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'header', children, attributes, classes);
+}
+
+Element footer(Object key, [List<Node> children = null, Map<String,
+    String> attributes = null, List<String> classes = null]) {
+  return new Element(key, 'footer', children, attributes, classes);
+}

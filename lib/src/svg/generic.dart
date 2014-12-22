cc6038f6b00afee4844a1a338f587ada63208a38
Boris Kaul <localvoid@gmail.com>
2014-12-22 21:35:24 +0600

    Fix library name

--- lib/src/svg/generic.dart
+++ lib/src/svg/generic.dart
@@ -2,7 +2,7 @@
 // details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-library vdom.html.generic;
+library vdom.svg.generic;
 
 import 'dart:svg' as svg;
 import '../context.dart';

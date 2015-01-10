9eb18fc63acc668d0b9c49dd2bb9790478d2611e
Boris Kaul <localvoid@gmail.com>
2015-01-11 01:15:53 +0600

    Fix library name

--- lib/src/html/video.dart
+++ lib/src/html/video.dart
@@ -2,7 +2,7 @@
 // details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-library vdom.html.img;
+library vdom.html.video;
 
 import 'dart:html' as html;
 import '../context.dart';

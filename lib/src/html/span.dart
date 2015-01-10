2ca68a55d8d73429325b34fbec7b6f062651a58d
Boris Kaul <localvoid@gmail.com>
2015-01-11 01:16:09 +0600

    Add Div and Span Html Elements

---
+++ lib/src/html/span.dart
@@ -0,0 +1,32 @@
+// Copyright (c) 2014, the VDom project authors. Please see the AUTHORS file for
+// details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+library vdom.html.span;
+
+import 'dart:html' as html;
+import '../context.dart';
+import '../node.dart';
+import 'element.dart';
+
+/// Virtual Html Element `<span>`
+class VSpan extends VHtmlElement<html.SpanElement> {
+  VSpan({
+    Object key,
+    List<VNode> children,
+    String id,
+    String type,
+    Map<String, String> attributes,
+    List<String> classes,
+    Map<String, String> styles})
+    : super(
+        key: key,
+        children: children,
+        id: id,
+        type: type,
+        attributes: attributes,
+        classes: classes,
+        styles: styles);
+
+  void create(Context context) { ref = new html.SpanElement(); }
+}
